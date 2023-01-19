6acb17ccf74c5724a1e6c6a576aada610614a2a7
Thiago Sprint <63547477+h4ckd0n@users.noreply.github.com>
2023-01-19 17:03:30 -0300

    Update somado_base.py
    meus testes!

--- somado_base.py
+++ somado_base.py
@@ -1,8 +1,8 @@
 from socket import if_nameindex
 
-
 num1 = int(input("Digite um valor:  "))
 num2 = int(input("Digite outro valor:  "))
 soma = num1+num2
+msg = str
 print("Resultado:   ",soma)
-
+print("Teste mais uma vez")
